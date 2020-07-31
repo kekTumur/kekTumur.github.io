577d3a6287d7e4210b5a2fcbcb72ea0272f291a6
Egan <58517845+kekTumur@users.noreply.github.com>
2020-07-31 11:36:10 +0300

    Add files via upload

---
+++ react-app/precache-manifest.46d4bfe9b3ed1808503e5e52335c378a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e821f68db622784c0f93f2f8727d01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fb97c3465cc717339f3",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "43d259c12720f42f4669",
+    "url": "/static/css/main.dbb0af7b.chunk.css"
+  },
+  {
+    "revision": "9fb97c3465cc717339f3",
+    "url": "/static/js/2.622a39ce.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.622a39ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43d259c12720f42f4669",
+    "url": "/static/js/main.593706c0.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
