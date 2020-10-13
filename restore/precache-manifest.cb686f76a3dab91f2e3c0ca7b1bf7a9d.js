7254e5c7144a1d847140c0791448d873b174a507
Egan <58517845+kekTumur@users.noreply.github.com>
2020-10-13 21:54:12 +0300

    Add files via upload

---
+++ restore/precache-manifest.cb686f76a3dab91f2e3c0ca7b1bf7a9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66873a9fbed3cadc7026adea65b1853",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7295530b83fe6cfbf46",
+    "url": "/static/css/main.17a0cbd0.chunk.css"
+  },
+  {
+    "revision": "c277b196ea1e38b7889a",
+    "url": "/static/js/2.14c5b23e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.14c5b23e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7295530b83fe6cfbf46",
+    "url": "/static/js/main.1cba2f2d.chunk.js"
+  },
+  {
+    "revision": "637c522a62e94c7046a4",
+    "url": "/static/js/runtime-main.aa6b9475.js"
+  }
+]);
